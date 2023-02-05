9d352b0b40b8c72cef30f2095aea973ceec55fbd
Jay Torry <jt847@cam.ac.uk>
2023-02-05 18:23:13 +0000

    Added OS-agnostic file handling using pathlib
    Simplified NaN check and fixed NaN count > 100% for multi-layer arrays
    Moved NaN check into tiling function and sped up processing time
    
    Moved regional 'AP'/'WS' prefix to input parameter for transparency
    Added 'discarded_tiles' to return values in case needed by outer script
    
    Changed output file prefixes from 'ftrs'/'lbls' to 'SAR'/'CHART'
    Allows for additional features / labels to be included in future
    
    Exposed output config, subfolders, file extension etc. as variables
    Added creation of output folders if don't already exist
    
    img_n counter now starts at zero (with +1 added for file naming convention)
    img_n also reported to stdout (along with discarded_tiles)
    
    Minor updates to var names for clarity, e.g. 'data_folder' -> 'output_folder'
    Minor updates to comments, docstrings etc.
    
    Various changes to example script, e.g.:
    Removal of trailing 'T' in date identifier for file names
    Removal of trailing '/' from output folder (no longer needed)
    Inclusion of region_prefix argument in call to tiling function
    
    BREAKING CHANGES:
    
    Output files will change due to updated NaN check
    File names will also be different due to prefix labelling etc.
    
    New 'region_prefix' argument must be added to tile_raster calls
    Trailing '/' must be removed from specified output folder path

--- code/tiling.py
+++ code/tiling.py
@@ -1,72 +1,57 @@
+'''
+AI4ER GTC - Sea Ice Classification
+
+Functions for loading and tiling of raster files
+
+'''
 
 import xarray  as xr
 import rioxarray as rxr
 from xarray.core.dataarray import DataArray
+from pathlib import Path
 
 
 def load_raster(file_path: str, parse_coordinates: bool=False, default_name: str=None, crs: int=4326) -> DataArray:
 
     '''
-    Loads and returns an xarray.core.dataarray.DataArray
+    Loads and returns xarray.core.dataarray.DataArray for a given raster file
 
             Parameters:
                     file_path (str): Path to file
                     parse_coordinates (bool): Parses the coordinates of the file, if any
                     default_name (str): Name for the array
-                    crs (int): CRS of the resulting array
+                    crs (int): Coordinate Reference System
 
             Returns:
                     raster (xarray.core.dataarray.DataArray): DataArray from file
     '''
 
-    raster = rxr.open_rasterio(file_path, parse_coordinates=parse_coordinates, default_name=default_name)
+    raster = rxr.open_rasterio(Path(file_path), parse_coordinates=parse_coordinates, default_name=default_name)
 
     assert type(raster) == xr.core.dataarray.DataArray # Makes sure the data structure is DataArray
-    raster.rio.write_crs(crs, inplace=True) # Modifies CRS of raster files
+    raster.rio.write_crs(crs, inplace=True) # Modifies CRS of the raster
 
     return raster
 
-def check_null_values(dataArray: DataArray, nan_threshold: float, size_x: int, size_y: int) -> bool:
 
-    '''
-    Loads and returns an xarray.core.dataarray.DataArray
-
-            Parameters:
-                    dataArray (xarray.core.dataarray.DataArray): DataArray object from the xarray library
-                    nan_threshold (float): number in [0,1]
-                    size_x (int): Size of the tile in the horizontal axis
-                    size_y (int): Size of the tile in the vertical axis
-
-            Returns:
-                    ans (bool): Whether the number of Nans is greater than the input threshold
-    '''
-
-    assert 0.0 <= nan_threshold <= 1.0  # Makes sure the parameter falls within the desired range
-
-    sub_null = dataArray.isnull() # Checks null values in array
-    groups = sub_null.groupby(sub_null).groups # Makes a dictionary of positions of Nan and non-Nan values
-    na_count = {key: len(groups[key]) for key in groups.keys()} # Counts Nan and non-Nan values
-
-    # Checks if the number of Nans in the raster is less than the threshold, if any
-    ans = (True in groups.keys() and na_count[True] / (size_x * size_y) < nan_threshold) or True not in groups.keys()
-
-    return ans
-
-def tile_raster(sar_image: DataArray, ice_chart: DataArray, data_folder: str, basename: str,
+def tile_raster(sar_image: DataArray, ice_chart: DataArray, output_folder: str, basename: str, region_prefix: str,
                 size_x: int=256, size_y: int=256, start_x: int=0, start_y: int=0,
                 end_x: int=None, end_y: int=None, stride_x: int=128, stride_y: int=128,
-                nan_threshold: float=1.0) -> int:
+                nan_threshold: float=1.0) -> tuple[int,int]:
 
     '''
-    Loads and returns an xarray.core.dataarray.DataArray
+    Slices a given pair of source images using a moving window, outputs valid tiles / sub-images to disk
+    Invalid tile pairs are skipped, e.g. where one or more of the pair contains an unacceptable number of NaN values
 
             Parameters:
-                    sar_image (xarray.core.dataarray.DataArray): DataArray object from the xarray library
-                    ice_chart (xarray.core.dataarray.DataArray): DataArray object from the xarray library
-                    data_folder (str): Path to folder where images will be saved
+                    sar_image (xarray.core.dataarray.DataArray): Original SAR image to be tiled
+                    ice_chart (xarray.core.dataarray.DataArray): Corresponding rasterised ice chart to be tiled
+                    output_folder (str): Path to folder where tiled images will be saved
                     basename (str): Base name for saved files
-                    size_x (int): Size of the tile in the horizontal axis
-                    size_y (int): Size of the tile in the vertical axis
+                    region_prefix (str): 2-character prefix indicating geographical region of the source files
+                        e.g. 'WS' = (East) Weddell Sea region, 'AP' = Antarctic Peninsula region
+                    size_x (int): Tile size in the horizontal axis
+                    size_y (int): Tile size in the vertical axis
                     start_x (int): Starting coordinate for tiling in the horizontal axis
                     start_y (int): Starting coordinate for tiling in the vertical axis
                     end_x (int): Final coordinate for tiling in the horizontal axis
@@ -76,8 +61,16 @@ def tile_raster(sar_image: DataArray, ice_chart: DataArray, data_folder: str, ba
                     nan_threshold (float): number in [0,1]
 
             Returns:
-                    img_n (int): Number of images generated
+                    img_n (int): Number of image / tile pairs generated
+                    discared_tiles (int): Number of tile pairs discarded
     '''
+
+    # Standard output config
+    features_subfolder = 'features'
+    labels_subfolder = 'labels'
+    sar_prefix = 'SAR'
+    chart_prefix = 'CHART'
+    output_ext = 'tiff'
 
     # Makes sure some parameters have default values according to input
     end_x = sar_image.shape[2] if end_x is None else end_x
@@ -86,6 +79,7 @@ def tile_raster(sar_image: DataArray, ice_chart: DataArray, data_folder: str, ba
     stride_y = size_y if stride_y is None else stride_y
 
     # Checkpoint of parameters
+    assert len(region_prefix) == 2
     assert 0 < size_x <= sar_image.shape[2] // 2
     assert 0 < size_y <= sar_image.shape[1] // 2
     assert 0 <= start_x <= sar_image.shape[2]
@@ -94,58 +88,67 @@ def tile_raster(sar_image: DataArray, ice_chart: DataArray, data_folder: str, ba
     assert 0 < end_y <= sar_image.shape[1]
     assert 0 < stride_x <= size_x
     assert 0 < stride_y <= size_y
+    assert 0.0 <= nan_threshold <= 1.0
 
-    # Counter for naming convention
-    img_n = 1
-    # Counter for discarded tiles
-    discarded_tiles = 0
+    # Create output dirs if they don't exist
+    Path.mkdir(Path(f'{output_folder}/{features_subfolder}'), parents=True, exist_ok=True)
+    Path.mkdir(Path(f'{output_folder}/{labels_subfolder}'), parents=True, exist_ok=True)
 
-    # Iterates over rows and columns of the image according to input parameters
+    img_n = 0 # Counter for image pairs generated (+1 for file naming convention)
+    discarded_tiles = 0 # Counter for discarded tile pairs
+
+    # Iterates over rows and columns of both images according to input parameters
     for row in range(start_y, end_y, stride_y):
         for col in range(start_x, end_x, stride_x):
 
-            # Tile naming convention
-            # Adding a prefix to distinguish data from the Weddell Sea and data from the Antarctic Peninsula 
-            if basename in ['20181220', '20181210', '20181209', '20181203']:
-                prefix = 'AP'
-            else:
-                prefix = 'WS'
-
-            file_n = '{:0>5}'.format(img_n)
-            filename = f'{prefix}_{basename}_{file_n}_[{col},{row}]_{size_x}x{size_y}.tiff'
-
-            # Indexes ice chart and SAR images according to parameters
+            # Indexes ice chart and SAR image for the current tile
             sub_sar = sar_image[:, row:row + size_y, col:col + size_x]
             sub_chart = ice_chart[:, row:row + size_y, col:col + size_x]
 
-            # Checks if the current tile has the same shape as the parameters, if not it continues
+            # Checks if the current tile has the same shape as the parameters, if not it skips to the next tile
             if (sub_sar.shape[1] * sub_sar.shape[2] != size_x * size_y) or (sub_chart.shape[1] * sub_chart.shape[2] != size_x * size_y):
-
+                
                 continue
 
-            # Checks Nan values in the current tile for both the ice chart and the SAR image. 
-            # If both are True, It will save the tiles in two different folders
-            if not check_null_values(sub_sar, nan_threshold, size_x, size_y) or not check_null_values(sub_chart, nan_threshold, size_x, size_y):
+            # NaN Check: Skip to next pair if too many NaNs in either tile
+            # TBC: Only checks first two layers of SAR tile (i.e. ignores incidence angle)
+            if (sub_chart.isnull().sum().values / (size_x*size_y) >= nan_threshold
+                    or sub_sar[0,:,:].isnull().sum().values / (size_x*size_y) >= nan_threshold
+                    or sub_sar[1,:,:].isnull().sum().values / (size_x*size_y) >= nan_threshold):
                 discarded_tiles += 1
                 continue
 
-            # Save files to different folders
-            sub_sar.rio.to_raster(data_folder + f'features/ftrs_{filename}')
-            sub_chart.rio.to_raster(data_folder + f'labels/lbls_{filename}')
+            # Majority of filename is common to both sar and ice tiles
+            file_n = '{:0>5}'.format(img_n+1)
+            common_fname = f'{region_prefix}_{basename}_{file_n}_[{col},{row}]_{size_x}x{size_y}.{output_ext}'
 
-            # Increase counter for naming convention
+            # Separate by subfolder and prefix
+            pathout_sar = f'{output_folder}/{features_subfolder}/{sar_prefix}_{common_fname}' 
+            pathout_chart = f'{output_folder}/{labels_subfolder}/{chart_prefix}_{common_fname}' 
+
+            # Save to disk
+            sub_sar.rio.to_raster(Path(pathout_sar))
+            sub_chart.rio.to_raster(Path(pathout_chart))
+
             img_n += 1
 
-    print (f"Number of discarded tiles: {discarded_tiles}")
-    return img_n
+    print (f"Number of image pairs generated: {img_n}")
+    print (f"Number of discarded tile pairs: {discarded_tiles}")
+
+    return img_n, discarded_tiles
 
 
 # # EXAMPLE
 # import re
 # from constants import chart_sar_pairs
 
-# ex_chart_image = load_raster('FTP_data/rasterised_shapefiles/' + chart_sar_pairs[0][0] + '.tiff', default_name='Ice Chart')
-# ex_sar_image = load_raster('FTP_data/dual_band_images/' + chart_sar_pairs[0][1] + '.tif', default_name='SAR Image')
-# name_extract = re.findall('H_[0-9]{8}T', chart_sar_pairs[0][1])
-# new_title = re.sub('H_', '', name_extract[0])
-# tile_raster(ex_sar_image, ex_chart_image, 'tiled_images/', new_title, size_x=256, size_y=256)
+# chart_folder = 'FTP_data/rasterised_shapefiles'
+# sar_folder = 'FTP_data/dual_band_images'
+# output_folder = 'Tiled_images'
+
+# ex_chart_image = load_raster(chart_folder + '/' + chart_sar_pairs[0][0] + '.tiff', default_name='Ice Chart')
+# ex_sar_image = load_raster(sar_folder + '/' + chart_sar_pairs[0][1] + '.tif', default_name='SAR Image')
+# region_prefix = chart_sar_pairs[0][2]
+# name_extract = re.findall('H_[0-9]{8}T',chart_sar_pairs[0][1])[0][2:10]
+# print(f"\nTiling {name_extract} ...")
+# tile_raster(ex_sar_image, ex_chart_image, output_folder, name_extract, region_prefix, size_x=256, size_y=256)
