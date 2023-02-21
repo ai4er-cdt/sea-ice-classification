d76ee9940c2057aa8b9dd96a52c068cde3c123af
Andrew McDonald <andrewmcdonald27@gmail.com>
2023-02-21 18:34:24 +0000

    update trainer to provide hardcoded list of train/val/test files for now

--- train.py
+++ train.py
@@ -5,8 +5,9 @@ from torch import nn
 from torch.utils.data import DataLoader
 from pytorch_lightning.callbacks import ModelCheckpoint
 from util import SeaIceDataset
+from pathlib import Path
 from model import Segmentation, UNet
-from torchmetrics import IoU  # may be called Jaccard Index in newer versions of torchmetrics
+from torchmetrics import JaccardIndex  # may be called Jaccard Index in newer versions of torchmetrics
 
 
 if __name__ == '__main__':
@@ -14,11 +15,12 @@ if __name__ == '__main__':
     # parse command line arguments
     parser = ArgumentParser(description="Sea Ice Segmentation")
     parser.add_argument("--name", default="seaice", type=str, help="Name of wandb run")
+    parser.add_argument("--model", default="unet", type=str, help="Name of model to train")
     parser.add_argument('--gpu_id', default=-1, type=int, help="GPU id to train on")
     parser.add_argument('--n_filters', default=16, type=float, help="Number of convolutional filters in hidden layer")
     parser.add_argument('--learning_rate', default=0.05, type=float, help="Learning rate")
     parser.add_argument('--batch_size', default=128, type=int, help="Batch size")
-    parser.add_argument('--n_epochs', default=15, type=int, help="Number of epochs to fine-tune")
+    parser.add_argument('--max_epochs', default=15, type=int, help="Number of epochs to fine-tune")
     parser.add_argument('--seed', default=0, type=int, help="Numpy random seed")
     parser.add_argument('--precision', default=32, help="Precision for training. Options are 32 or 16")
     args = parser.parse_args()
@@ -28,12 +30,38 @@ if __name__ == '__main__':
 
     pl.seed_everything(args.seed)
 
-    # configure dataloaders
-    train_dataset = SeaIceDataset(sar_path="./sar", chart_path="./chart", transform=None)
+    base_folder = open("data_path.config").read().strip()
+    chart_folder = Path(f"../Tiled_images/binary_chart")
+    sar_folder = Path(f"../Tiled_images/sar")
+
+    # TODO: read in train files from csv somewhere
+    train_sar_files = ["SAR_AP_20181202_00050_[9600,256]_256x256.tiff", "SAR_AP_20181202_00051_[9728,256]_256x256.tiff"]
+    train_chart_files = ["BINARY_CHART_AP_20181202_00050_[9600,256]_256x256.tiff", "BINARY_CHART_AP_20181202_00051_[9728,256]_256x256.tiff"]
+    train_dataset = SeaIceDataset(sar_path=str(sar_folder),
+                                  sar_files=train_sar_files,
+                                  chart_path=str(chart_folder),
+                                  chart_files=train_chart_files,
+                                  transform=None)
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=4)
-    val_dataset = SeaIceDataset(sar_path="./sar", chart_path="./chart", transform=None)
+
+    # TODO: read in val files from csv somewhere
+    val_sar_files = ["SAR_AP_20181202_00052_[9856,256]_256x256.tiff", "SAR_AP_20181202_00053_[9984,256]_256x256.tiff"]
+    val_chart_files = ["BINARY_CHART_AP_20181202_00052_[9856,256]_256x256.tiff", "BINARY_CHART_AP_20181202_00053_[9984,256]_256x256.tiff"]
+    val_dataset = SeaIceDataset(sar_path=str(sar_folder),
+                                sar_files=val_sar_files,
+                                chart_path=str(chart_folder),
+                                chart_files=val_chart_files,
+                                transform=None)
     val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, num_workers=4)
-    test_dataset = SeaIceDataset(sar_path="./sar", chart_path="./chart", transform=None)
+
+    # TODO: read in test files from csv somewhere
+    test_sar_files = ["SAR_AP_20181202_00073_[9344,384]_256x256.tiff", "SAR_AP_20181202_00074_[9472,384]_256x256.tiff"]
+    test_chart_files = ["BINARY_CHART_AP_20181202_00073_[9344,384]_256x256.tiff", "BINARY_CHART_AP_20181202_00074_[9472,384]_256x256.tiff"]
+    test_dataset = SeaIceDataset(sar_path=str(sar_folder),
+                                 sar_files=test_sar_files,
+                                 chart_path=str(chart_folder),
+                                 chart_files=test_chart_files,
+                                 transform=None)
     test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, num_workers=4)
 
     # configure model
@@ -42,7 +70,7 @@ if __name__ == '__main__':
     else:
         raise ValueError("Unsupported model type")
     criterion = nn.CrossEntropyLoss()
-    metric = IoU(num_classes=2)
+    metric = JaccardIndex(task="binary")
     segmenter = Segmentation(train_dataloader, val_dataloader, model, criterion, args.learning_rate, metric)
 
     # set up wandb logging
@@ -54,7 +82,7 @@ if __name__ == '__main__':
     wandb_logger.experiment.config.update(args)
 
     # set up trainer configuration
-    trainer = pl.Trainer()
+    trainer = pl.Trainer.from_argparse_args(args)
     trainer.logger = wandb_logger
     trainer.callbacks.append(ModelCheckpoint(monitor="val_loss"))
 
