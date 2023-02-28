90ae2846468cac4448929af0a4b78a51b516d83f
Andrés Camilo Zúñiga González <33363093+ancazugo@users.noreply.github.com>
2023-02-28 19:58:21 +0000

    Add new_classes and classification type arguments

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import os
 import pytorch_lightning as pl
 import wandb
 from argparse import ArgumentParser
+from constants import new_classes
 from torch import nn
 from torch.utils.data import DataLoader
 from pytorch_lightning.callbacks import ModelCheckpoint
@@ -20,6 +21,7 @@ if __name__ == "__main__":
     parser.add_argument("--name", default="3i9dp51u", type=str, help="Name of wandb run")
     parser.add_argument("--batch_size", default=128, type=int, help="Batch size")
     parser.add_argument("--overfit", default=False, type=eval, help="Whether or not to overfit on a single image")
+    parser.add_argument("--classification_type", default=None, type=str, help="Binary, ternary or multiclass classification")
     args = parser.parse_args()
 
     # standard input dirs
@@ -34,12 +36,14 @@ if __name__ == "__main__":
         with open(Path(f"{base_folder}/test_files.txt"), "r") as f:
             test_files = f.read().splitlines()
     
+    class_categories = new_classes[args.classification_type]
+    
     # load test data
     test_sar_files = [f"SAR_{f}" for f in test_files]
-    test_chart_files = [f"BINARY_CHART_{f}" for f in test_files]
+    test_chart_files = [f"CHART_{f}" for f in test_files]
     test_dataset = SeaIceDataset(sar_path=sar_folder,sar_files=test_sar_files,
                                  chart_path=chart_folder,chart_files=test_chart_files,
-                                 transform=None)
+                                 transform=None,class_categories=class_categories)
     test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, num_workers=4)
 
     # wandb logging
@@ -58,12 +62,14 @@ if __name__ == "__main__":
         if epoch > best_epoch:
             best_epoch = epoch
             best_checkpoint = f"{checkpoint_folder}/{checkpoint}"
-    model = Segmentation.load_from_checkpoint(best_checkpoint, model=UNet(kernel=3, n_channels=3, n_filters=run.config["n_filters"], n_classes=2))
+    
+    n_classes = len(class_categories)
+    model = Segmentation.load_from_checkpoint(best_checkpoint, model=UNet(kernel=3, n_channels=3, n_filters=run.config["n_filters"], n_classes=n_classes))
 
     # test
     model.eval()
     criterion = model.criterion
-    metric = JaccardIndex(task="binary")  # !! should this be multiclass? 
+    metric = JaccardIndex(task="multiclass")
     losses = []
     metrics = []
     for batch in test_dataloader:
