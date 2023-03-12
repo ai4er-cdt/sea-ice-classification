13b39daba8441ec8a603d14b1e8c1c1e7e0d2a81
Andrew McDonald <andrewmcdonald27@gmail.com>
2023-03-12 12:48:48 +0000

    fix r2 metric and remove deprecated debugging code

--- model.py
+++ model.py
@@ -52,11 +52,11 @@ class Segmentation(pl.LightningModule):
             "micro_f1": F1Score(task="multiclass", num_classes=n_classes, average="micro"),
             "macro_f1": F1Score(task="multiclass", num_classes=n_classes, average="macro"),
             "weighted_f1": F1Score(task="multiclass", num_classes=n_classes, average="weighted"),
-            "r_squared": R2Score(),  # regression-based metrics for ternary and multiclass models
             "mean_squared_error": MeanSquaredError(squared=True),
             "root_mean_squared_error": MeanSquaredError(squared=False),
             "mean_absolute_error": MeanAbsoluteError()
         })
+        self.r2_score = R2Score()  # requires flattening inputs
 
         self.save_hyperparameters(ignore=["model", "criterion"])
 
@@ -76,14 +76,6 @@ class Segmentation(pl.LightningModule):
         :return: Loss from this batch of data for use in backprop
         """
         x, y = batch["sar"], batch["chart"].squeeze().long()
-        if y.min() < 0 or y.max() > 2:
-            yy = y.view(y.shape[0], -1)  # reshape into batch_size x n_pixels
-            mins, min_indices = yy.min(dim=1)  # get min value per image in batch
-            maxs, max_indices = yy.max(dim=1)  # get max value per image in batch
-            to_investigate = torch.logical_or(mins < 0, maxs > 2).nonzero().cpu().ravel().tolist()
-            print("Target out of range")
-            print(batch["chart_name"])
-            print(to_investigate)
         y_hat = self.model(x)
         loss = self.criterion(y_hat, y)
         self.log("train_loss", loss)
@@ -91,18 +83,11 @@ class Segmentation(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, y = batch["sar"], batch["chart"].squeeze().long()
-        if y.min() < 0 or y.max() > 2:
-            yy = y.view(y.shape[0], -1)  # reshape into batch_size x n_pixels
-            mins, min_indices = yy.min(dim=1)  # get min value per image in batch
-            maxs, max_indices = yy.max(dim=1)  # get max value per image in batch
-            to_investigate = torch.logical_or(mins < 0, maxs > 2).nonzero().cpu().ravel().tolist()
-            print("Target out of range")
-            print(batch["chart_name"])
-            print(to_investigate)
         y_hat = self.model(x)
         loss = self.criterion(y_hat, y)
         y_hat_pred = y_hat.argmax(dim=1)
         self.metrics.update(y_hat_pred, y)
+        self.r2_score.update(y_hat_pred.view(-1), y.view(-1))
         return loss
 
     def validation_epoch_end(self, outputs):
@@ -117,6 +102,7 @@ class Segmentation(pl.LightningModule):
         loss = self.criterion(y_hat, y)
         y_hat_pred = y_hat.argmax(dim=1)
         self.metrics.update(y_hat_pred, y)
+        self.r2_score.update(y_hat_pred.view(-1), y.view(-1))
         return loss
 
     def testing_epoch_end(self, outputs):
