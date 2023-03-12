77c12f9ee8f6f5e3ea2cdf4ad804a6b0cc7444a8
ampersandmcd <andrewmcdonald27@gmail.com>
2023-03-12 21:01:12 +0000

    training goes brrrrrrrrrrrrr

--- train_slurm_script.sh
+++ train_slurm_script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --partition=orchid
 #SBATCH --account=orchid
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 #SBATCH --time=24:00:00
-#SBATCH --ntasks=32
-#SBATCH --ntasks-per-node=32
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=32000
 
 # executables
 conda activate sea-ice-classification
-
-python train.py --model=resnet34 --classification_type=binary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=ratio --n_workers=0
+nvidia-smi
+python train.py --model=resnet34 --classification_type=binary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=ratio --n_workers=4 --devices=4
