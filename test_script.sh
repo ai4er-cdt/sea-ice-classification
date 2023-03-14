01262fdcb1265ec470711e7a4252a9b4440d1b59
Andrew McDonald <andrewmcdonald27@gmail.com>
2023-03-14 08:58:52 +0000

    add template test script

---
+++ test_script.sh
@@ -0,0 +1,19 @@
+# (u-net, binary, angle)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
+# (resnet, binary, angle)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
+
+# (u-net, binary, ratio)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
+# (resnet, binary, angle)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
+
+# (u-net, ternary, angle)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
+# (resnet, ternary, angle)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
+
+# (u-net, ternary, ratio)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
+# (resnet, ternary, angle)
+python test.py --username=TODO --name=TODO --checkpoint="" --n_workers=4
