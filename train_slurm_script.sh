5627f968668db5056ca5cee243679492e5c90caa
maplumridge <map205@cam.ac.uk>
2023-03-16 11:41:05 +0000

    Adding all executed model training runs

--- train_slurm_script.sh
+++ train_slurm_script.sh
@@ -13,4 +13,27 @@
 # executables
 conda activate sea-ice-classification
 nvidia-smi
+
+# Unet, Binary, Angle
+python train.py --model=unet --classification_type=binary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=angle --n_workers=4 --devices=4 --max_epochs=20
+
+# Resnet34, Binary, Angle
+python train.py --model=resnet34 --classification_type=binary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=angle --n_workers=4 --devices=4 --max_epochs=20
+
+# Unet, Binary, Ratio
+python train.py --model=unet --classification_type=binary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=ratio --n_workers=4 --devices=4 --max_epochs=20
+
+# Resnet34, Binary, Ratio
 python train.py --model=resnet34 --classification_type=binary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=ratio --n_workers=4 --devices=4 --max_epochs=20
+
+# Unet, Ternary, Angle
+python train.py --model=unet --classification_type=ternary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=angle --n_workers=4 --devices=4 --max_epochs=20
+
+# Resnet34, Ternary, Angle
+python train.py --model=resnet34 --classification_type=ternary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=angle --n_workers=4 --devices=4 --max_epochs=20
+
+# Unet, Ternary, Ratio
+python train.py --model=unet --classification_type=ternary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=ratio --n_workers=4 --devices=4 --max_epochs=20
+
+# Resnet34, Ternary, Ratio
+python train.py --model=resnet34 --classification_type=ternary --criterion=ce --batch_size=256 --learning_rate=1e-3 --seed=0 --sar_band3=ratio --n_workers=4 --devices=4 --max_epochs=20
